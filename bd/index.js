41b2787a4074909de0442ce8ca6633628e2aa6d5
FIROZ KHAN <computerpc096@gmail.com>
2025-04-17 06:51:39 -0500

    ok

--- bd/index.js
+++ bd/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: ["http://localhost:5173", "https://kings-food.vercel.app/"],
   credentials: true,
 }));
 // Routes
